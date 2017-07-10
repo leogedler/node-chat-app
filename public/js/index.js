af18785cc930bcf01a20a804d599305d27bb07e5
leonardogedler <leogedler@hotmail.com>
2017-07-09 21:08:15 -0400

    Greet new user

--- public/js/index.js
+++ public/js/index.js
@@ -11,3 +11,7 @@ socket.on('disconnect', function(){
 socket.on('newMessage', function(message){
     console.log('New Message', message);
 });
+
+socket.on('newUser', function(message){
+    console.log(message);
+})
